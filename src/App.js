4572b0b510f8fc50b27df8250976a911fbb43acf
My Hanh <myhanh@Mys-MacBook-Air.local>
2022-07-03 14:18:54 +0700

    clean-architechture

--- src/App.js
+++ src/App.js
@@ -1,75 +1,18 @@
+import React from "react";
+
 import "./App.scss";
+
+//custom components
+import AppBar from "components/AppBar/AppBar";
+import BoardBar from "components/BoardBar/BoardBar";
+import BoardContent from "components/BoardContent/BoardContent";
 
 function App() {
   return (
     <div className="trello-web">
-      <nav className="navbar app">App bar</nav>
-      <nav className="navbar board">board bar</nav>
-      <div className="board-columns">
-        <div className="column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img src="https://i.insider.com/61d1c0e2aa741500193b2d18?width=1136&format=jpeg" alt="cat" />
-              Title: MyHanh
-            </li>
-            <li>add a new line</li>
-            <li>add a new line</li>
-            <li>add a new line</li>
-            <li>add a new line</li>
-            <li>add a new line</li>
-            <li>add a new line</li>
-            <li>add a new line</li>
-            <li>add a new line</li>
-            <li>add a new line</li>
-            <li>add a new line</li>
-          </ul>
-
-          <footer>Add another card</footer>
-        </div>
-        <div className="column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img src="https://i.insider.com/61d1c0e2aa741500193b2d18?width=1136&format=jpeg" alt="cat" />
-              Title: MyHanh
-            </li>
-            <li>add a new line</li>
-            <li>add a new line</li>
-            <li>add a new line</li>
-            <li>add a new line</li>
-            <li>add a new line</li>
-            <li>add a new line</li>
-            <li>add a new line</li>
-            <li>add a new line</li>
-            <li>add a new line</li>
-            <li>add a new line</li>
-          </ul>
-
-          <footer>Add another card</footer>
-        </div>
-        <div className="column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img src="https://i.insider.com/61d1c0e2aa741500193b2d18?width=1136&format=jpeg" alt="cat" />
-              Title: MyHanh
-            </li>
-            <li>add a new line</li>
-            <li>add a new line</li>
-            <li>add a new line</li>
-            <li>add a new line</li>
-            <li>add a new line</li>
-            <li>add a new line</li>
-            <li>add a new line</li>
-            <li>add a new line</li>
-            <li>add a new line</li>
-            <li>add a new line</li>
-          </ul>
-
-          <footer>Add another card</footer>
-        </div>
-      </div>
+      <AppBar/>
+      <BoardBar/>
+      <BoardContent/>
     </div>
   );
 }
