c93eaddf5dbe42b41d0f5be509709113f3635c2a
My Hanh <myhanh@Mys-MacBook-Air.local>
2022-07-03 19:17:25 +0700

    initial sample data

---
+++ src/actions/initalData.js
@@ -0,0 +1,160 @@
+export const initalData = {
+    boards: [
+        {
+            id: 'board-1',
+            columnOrder: ['column-2', 'column-1', 'column-3'],
+            columns: [
+                {
+                    id: 'column-1',
+                    boardId: 'board-1',
+                    title: 'Todo column 1',
+                    cardOrder:['card-1', 'card-2', 'card-3','card-4', 'card-5', 'card-6'],
+                    cards: [
+                        {
+                            id: 'card-1',
+                            boardId: 'board-1',
+                            columnId: 'column-1',
+                            title: 'title card 1',
+                            cover: null
+                        },
+                        {
+                            id: 'card-2',
+                            boardId: 'board-1',
+                            columnId: 'column-1',
+                            title: 'title card 1',
+                            cover: null
+                        },
+                        {
+                            id: 'card-3',
+                            boardId: 'board-1',
+                            columnId: 'column-1',
+                            title: 'title card 1',
+                            cover: null
+                        }, 
+                        {
+                            id: 'card-4',
+                            boardId: 'board-1',
+                            columnId: 'column-1',
+                            title: 'title card 1',
+                            cover: null
+                        },
+                        {
+                            id: 'card-5',
+                            boardId: 'board-1',
+                            columnId: 'column-1',
+                            title: 'title card 1',
+                            cover: null
+                        },
+                        {
+                            id: 'card-6',
+                            boardId: 'board-1',
+                            columnId: 'column-1',
+                            title: 'title card 1',
+                            cover: null
+                        }
+                    ]
+                },
+                {
+                    id: 'column-2',
+                    boardId: 'board-1',
+                    title: 'Todo column 2',
+                    cardOrder:['card-11', 'card-12', 'card-13','card-14', 'card-15', 'card-16'],
+                    cards: [
+                        {
+                            id: 'card-11',
+                            boardId: 'board-1',
+                            columnId: 'column-2',
+                            title: 'title card 1',
+                            cover: 'https://i.insider.com/61d1c0e2aa741500193b2d18?width=1136&format=jpeg'
+                        },
+                        {
+                            id: 'card-12',
+                            boardId: 'board-1',
+                            columnId: 'column2',
+                            title: 'title card 1',
+                            cover: null
+                        },
+                        {
+                            id: 'card-13',
+                            boardId: 'board-1',
+                            columnId: 'column-2',
+                            title: 'title card 1',
+                            cover: null
+                        }, 
+                        {
+                            id: 'card-14',
+                            boardId: 'board-1',
+                            columnId: 'column-2',
+                            title: 'title card 1',
+                            cover: null
+                        },
+                        {
+                            id: 'card-15',
+                            boardId: 'board-1',
+                            columnId: 'column-2',
+                            title: 'title card 1',
+                            cover: null
+                        },
+                        {
+                            id: 'card-16',
+                            boardId: 'board-1',
+                            columnId: 'column-2',
+                            title: 'title card 1',
+                            cover: null
+                        }
+                    ]
+                },
+                {
+                    id: 'column-3',
+                    boardId: 'board-1',
+                    title: 'Todo column 3',
+                    cardOrder:['card-16', 'card-12', 'card-13','card-14', 'card-15', 'card-11'],
+                    cards: [
+                        {
+                            id: 'card-11',
+                            boardId: 'board-1',
+                            columnId: 'column-2',
+                            title: 'title card 1',
+                            cover: 'https://i.insider.com/61d1c0e2aa741500193b2d18?width=1136&format=jpeg'
+                        },
+                        {
+                            id: 'card-12',
+                            boardId: 'board-1',
+                            columnId: 'column2',
+                            title: 'title card 1',
+                            cover: null
+                        },
+                        {
+                            id: 'card-13',
+                            boardId: 'board-1',
+                            columnId: 'column-2',
+                            title: 'title card 1',
+                            cover: null
+                        }, 
+                        {
+                            id: 'card-14',
+                            boardId: 'board-1',
+                            columnId: 'column-2',
+                            title: 'title card 1',
+                            cover: null
+                        },
+                        {
+                            id: 'card-15',
+                            boardId: 'board-1',
+                            columnId: 'column-2',
+                            title: 'title card 1',
+                            cover: null
+                        },
+                        {
+                            id: 'card-16',
+                            boardId: 'board-1',
+                            columnId: 'column-2',
+                            title: 'title card 1',
+                            cover: null
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
+}
--- src/components/BoardContent/BoardContent.js
+++ src/components/BoardContent/BoardContent.js
@@ -1,14 +1,37 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
+import {isEmpty} from 'lodash'
 
 import './BoardContent.scss'
 import Column from "components/Column/Column"
+import { mapOrder } from "utilities/sort"
+import { initalData } from "actions/initalData"
 
-function BoardContent(){
+function BoardContent() {
+    const [board, setBoard] = useState({})
+    const [columns, setColumns] = useState([])
+
+    useEffect(() => {
+        const boardFromDB = initalData.boards.find(board => board.id === 'board-1')
+        if(boardFromDB){
+            setBoard(boardFromDB)
+
+            // //sort  column
+            // boardFromDB.columns.sort(function(a,b){
+            //     return boardFromDB.columnOrder.indexOf(a.id) - boardFromDB.columnOrder.indexOf(b.id)
+            // })
+            setColumns(mapOrder(boardFromDB.columns, boardFromDB.columnOrder, 'id'))
+        }
+    }, [])
+
+    if(isEmpty(board)){
+        return <div className="not-found">Board not found!</div>
+    }
+
     return (
-<div className="board-content">
-       <Column/>
-       <Column/>
-      </div>
+        <div className="board-content">
+            {columns.map((column, index) => <Column key={index} column={column}/>)
+            }
+        </div>
     )
 
 }
